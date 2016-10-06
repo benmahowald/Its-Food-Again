55306ff14d00801830ce765ee042e3c15bc3431f
Ben Mahowald <ben.mahowald@gmail.com>
2016-10-06 18:34:35 -0500

    routing

--- public/ng-router/client.js
+++ public/ng-router/client.js
@@ -4,24 +4,23 @@ var myApp = angular.module('myApp', ["ngRoute"]);
 
 // config routes
 myApp.config(["$routeProvider", function($routeProvider){
-  $routeProvider.
-    when("/create",{
-      templateUrl:"/views/partials/createAccount.html",
+  $routeProvider
+    .when("/create", {
+      templateUrl: "/views/partials/createAccount.html",
       controller: "accountController"
-    }).
-    when("/map",{
-      templateUrl:"/views/partials/mapView.html",
+    })
+    .when("/map", {
+      templateUrl: "/views/partials/mapView.html",
       controller: "mapViewController"
-    }).
-    when("/post",{
-      templateUrl:"/views/partials/postFood.html",
+    })
+    .when("/post", {
+      templateUrl: "/views/partials/postFood.html",
       controller: "postFoodController"
-    }).
-    otherwise({
+    })
+    .otherwise( {
       redirectTo: "/"
     });
 }]);
-
 // var lock = new Auth0Lock('HHp6IJkxeJhf5IKHQ5NUAETGyq9ggCll', 'ben-mahowald.auth0.com');
 // // log out url, from Auth0
 // var logOutUrl = 'https://ben-mahowald.auth0.com/v2/logout';
