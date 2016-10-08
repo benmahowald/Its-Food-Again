641b6c4b38ff07413cca083b6f50512de9c38aef
Ben Mahowald <ben.mahowald@gmail.com>
2016-10-07 22:21:37 -0500

    login working

--- public/ng-router/controllers/homeController.js
+++ public/ng-router/controllers/homeController.js
@@ -2,72 +2,73 @@ var lock = new Auth0Lock('HHp6IJkxeJhf5IKHQ5NUAETGyq9ggCll', 'ben-mahowald.auth0
 // log out url, from Auth0
 var logOutUrl = 'https://ben-mahowald.auth0.com/v2/logout';
 
-myApp.controller('homeController', ['$scope', function($scope){
+myApp.controller('homeController', ['$scope', '$http', function($scope, $http){
 console.log('in home controller');
 $scope.map = true;
   $scope.showMap = function (){
     $scope.map = true;
-    console.log($scope.map);
+    // console.log($scope.map);
   }; // end showMap
   $scope.hideMap = function (){
     $scope.map = false;
   }; // end hideMap
 
-  // $scope.init = function(){
-  //   console.log( 'in init' );
-  //   // check if a user's info is saved in localStorage
-  //   if( JSON.parse( localStorage.getItem( 'userProfile' ) ) ){
-  //     // if so, save userProfile as $scope.userProfile
-  //     $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile' ) );
-  //     console.log( 'loggedIn:', $scope.userProfile );
-  //     $scope.showUser = true;
-  //   }
-  //   else{
-  //     // if not, make sure we are logged out and empty
-  //     emptyLocalStorage();
-  //     $scope.showUser = false;
-  //   }
-  // }; // end init function
-
-  // $scope.logIn = function(){
-  //   // call out logIn function from auth0.js
-  //   console.log( 'in logIn' );
-  //   lock.show( function( err, profile, token ) {
-  //     if (err) {
-  //       console.error( "auth error: ", err);
-  //     } // end error
-  //     else {
-  //       // save token to localStorage
-  //       localStorage.setItem( 'userToken', token );
-  //       // save user profile to localStorage
-  //       localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
-  //       // reload page because dirtyhaxorz
-  //       location.reload();
-  //     } // end no error
-  //   }); //end lock.show
-  // }; // end scope.logIn
-
-  // $scope.logOut = function(){
-  //   // call our logOutUrl
-  //   $http({
-  //     method:'GET',
-  //     url: logOutUrl,
-  //   }).then( function( data ){
-  //     // if logged out OK
-  //     if( data.data == 'OK' ){
-  //       // empty localStorage
-  //       emptyLocalStorage();
-  //       $scope.showUser = false;
-  //     }
-  //   })
-  // }; // end scope.logOut
+  $scope.init = function(){
+    console.log( 'in init' );
+    // check if a user's info is saved in localStorage
+    if( JSON.parse( localStorage.getItem( 'userProfile' ) ) ){
+      // if so, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile' ) );
+      console.log( 'loggedIn:', $scope.userProfile );
+      $scope.showUser = true;
+    }
+    else{
+      // if not, make sure we are logged out and empty
+      emptyLocalStorage();
+      $scope.showUser = false;
+    }
+  }; // end init function
 
   // run init on controller load
-  // $scope.init();
+  $scope.init();
+
+  $scope.logIn = function(){
+    // call out logIn function from auth0.js
+    console.log( 'in logIn' );
+    lock.show( function( err, profile, token ) {
+      if (err) {
+        console.error( "auth error: ", err);
+      } // end error
+      else {
+        // save token to localStorage
+        localStorage.setItem( 'userToken', token );
+        // save user profile to localStorage
+        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+        // reload page because dirtyhaxorz
+        location.reload();
+      } // end no error
+    }); //end lock.show
+  }; // end scope.logIn
+
+  $scope.logOut = function(){
+    // call our logOutUrl
+    $http({
+      method:'GET',
+      url: logOutUrl,
+    }).then( function( data ){
+      // if logged out OK
+      if( data.data == 'OK' ){
+        // empty localStorage
+        confirm('Are you sure you want to log out?');
+        emptyLocalStorage();
+        $scope.showUser = false;
+        console.log($scope.showUser);
+      } // end if
+    }); // end then
+  }; // end scope.logOut
 }]); // end authController
 
 var emptyLocalStorage = function(){
   localStorage.removeItem( 'userProfile' );
   localStorage.removeItem( 'userToken' );
 }; // end emptyLocalStorage
-}]); // end controller
